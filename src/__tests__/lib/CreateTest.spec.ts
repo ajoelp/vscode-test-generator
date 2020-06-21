6da0728fbd964e49678d88205f37e0eecb40187f
Joel Podrebarac <joelpodrebar@gmail.com>
2020-06-20 22:13:48 -0400

    Fix paths

---
+++ src/__tests__/lib/CreateTest.spec.ts
@@ -0,0 +1,44 @@
+import CreateTest from "../../lib/CreateTest";
+import * as fs from "fs-extra";
+import MockedVSCode from "../../lib/MockedVSCode";
+import * as path from "path";
+
+jest.mock("../../lib/Log", () => {
+  return {
+    error: (error: string) => {
+      throw error;
+    },
+  };
+});
+
+const relativePath = (dir: string) => path.resolve(process.cwd(), dir);
+
+describe("CreateTest", () => {
+  it("will create a file in a __tests__ directory", async () => {
+    const vscode = new MockedVSCode("./src", "./", "spec", "__tests__", false);
+    await CreateTest.create("./src/app/file.ts", vscode);
+    expect(fs.mkdirp).toHaveBeenCalled();
+    expect(fs.existsSync).toHaveBeenCalled();
+    expect(fs.writeFile).toHaveBeenCalledWith(
+      relativePath("__tests__/app/file.spec.ts"),
+      ""
+    );
+    expect(vscode.openFile).toHaveBeenCalledWith(
+      relativePath("__tests__/app/file.spec.ts")
+    );
+  });
+
+  it("will create a file in the same directory", async () => {
+    const vscode = new MockedVSCode("./src", "./", "spec", "__tests__", true);
+    await CreateTest.create("./src/app/file.ts", vscode);
+    expect(fs.mkdirp).toHaveBeenCalled();
+    expect(fs.existsSync).toHaveBeenCalled();
+    expect(fs.writeFile).toHaveBeenCalledWith(
+      relativePath("./src/app/file.spec.ts"),
+      ""
+    );
+    expect(vscode.openFile).toHaveBeenCalledWith(
+      relativePath("./src/app/file.spec.ts")
+    );
+  });
+});
